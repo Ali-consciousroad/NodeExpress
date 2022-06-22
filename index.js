5665f98627d1f96f0269dd3bcd1b50cfaaad209c
Ali-consciousroad <ali_dindar@live.be>
2022-06-22 20:52:00 +0200

    Assignment 1: Modularize and simplify the app for routes with parameterss, REST API endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hostname = 'localhost';
 const port = 3000;
 const app = express();
 // Body parsing middleware
+// Require the multiple modules needed
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
@@ -14,7 +15,7 @@ const leaderRouter = require('./routes/leaderRouter');
 // dishRouter = express.Router({ mergeParams: true });
 
 
-// Load the router modules 
+// Mount the routers on the required endpoints
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
@@ -30,68 +31,6 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-// /dishes/:dishId
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.get('/promotions/:promoId', (req,res,next) => {
-    res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
-});
-
-app.get('/leaders/:leaderId', (req,res,next) => {
-    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
-});
-
-// Create operation not allowed from this specific endpoint URL
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.post('/promotions/:promoId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /promotions/' + req.params.promoId);
-});
-
-app.post('/leaders/:leaderId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /leaders/' + req.params.leaderId);
-});
-
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
-});
-
-app.put('/promotions/:promoId', (req, res, next) => {
-    res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name +
-        ' with details: ' + req.body.description);
-});
-
-app.put('/leaders/:leaderId', (req, res, next) => {
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name +
-        ' with details: ' + req.body.description);
-});
-
-
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
-app.delete('/promotions/:promoId', (req, res, next) => {
-    res.end('Deleting promotion: ' + req.params.promoId);
-});
-
-app.delete('/leaders/:leaderId', (req, res, next) => {
-    res.end('Deleting leader: ' + req.params.leaderId);
-});
-
 
 const server = http.createServer(app);
 
